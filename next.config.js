726d72085e4ac504fabdaf459c4ac7e32af5b8c5
Nurizko Maulana <nurizko.maulana@gmail.com>
2023-10-18 12:39:49 +0700

    feat: redirect next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async redirects() {
+      return [
+          // Redirect /blog/*/ to /blog/*
+        {
+              source: '/blog/:path*/',
+              destination: 'http://3.25.172.84/:path*/',
+              permanent: true,
+          },
+      ]
+    },
+  }
 
 module.exports = nextConfig
